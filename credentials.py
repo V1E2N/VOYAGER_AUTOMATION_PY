98047fc5c18c736621b5f5bf4cd5cb23a8cd3a27
V1E2N <153701124+V1E2N@users.noreply.github.com>
2025-01-06 15:48:56 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,10 @@
+class credentials():
+    @staticmethod
+    def username():
+        return "Venkatesh.Muthu@vca.com"
+    @staticmethod
+    def password():
+        return "Venkat1997@kavi"
+    @staticmethod
+    def security_question():
+        return "pilot"
